2eb057d3d1cb258b979980d0a6a892551009e239
ImranGreet <hossainimran1208@gmail.com>
2025-10-30 15:31:55 +0600

    cabin model now following the ps-4 naming conventions

--- app/Models/HMS/Cabin.php
+++ app/Models/HMS/Cabin.php
@@ -9,4 +9,15 @@ class Cabin extends Model
 {
     /** @use HasFactory<\Database\Factories\HMS\CabinFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'cabin_number',
+        'type',
+        'floor_number',
+        'bed_count',
+        'price_per_day',
+        'is_occupied',
+        'patient_id',
+        'notes',
+    ];
 }
